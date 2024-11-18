5cfd88d8791543b8b461869974a63debaab15a5d
zahra706 <chebbizahra6@gmail.com>
2024-11-18 11:13:30 +0100

    Add .nojekyll and update workflow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FaceSnap } from './models/face-snap.model';
+
 
 @Component({
   selector: 'app-root',
@@ -7,23 +7,5 @@ import { FaceSnap } from './models/face-snap.model';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  mySnaps!: FaceSnap[];
-  ngOnInit():void{
-    this.mySnaps=[
-      new FaceSnap(
-    'archi',
-    "my BFF",
-    new Date(),
-    6,
-    'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
-    ),
-    new FaceSnap(
-      'baby',
-      "beiber",
-      new Date(),
-      5,
-      'https://cdn.pixabay.com/photo/2015/05/31/16/03/tedd-bear-792273_1280.jpg',
-      ),
-  ];
-  }
+  
 }
